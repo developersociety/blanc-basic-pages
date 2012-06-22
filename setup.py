4a409aeedfe2fcce2c27ab9ef204a2bbf281ebab
Alex Tomkins <tomkins@darkzone.net>
2012-06-22 16:22:36 +0100

    Update to 0.1.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 setup(
     name='blanc-basic-pages',
-    version='0.1',
+    version='0.1.1',
     description='Blanc Basic Pages for Django',
     long_description=open('README.rst').read(),
     url='http://www.blanctools.com/',
