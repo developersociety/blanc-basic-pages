0e44b1a76f9d40f4932e79dda1a8b7527311fda2
Alex Tomkins <tomkins@darkzone.net>
2012-12-13 12:56:57 +0000

    Can't use get_page_model with signals

--- blanc_basic_pages/pages/listeners.py
+++ blanc_basic_pages/pages/listeners.py
@@ -1,10 +1,14 @@
 from django.db.models.signals import post_save
+from django.conf import settings
 from django.contrib.sites.models import Site
-from blanc_basic_pages.pages import get_page_model
+from blanc_basic_pages.pages import DEFAULT_MODEL
 
 
 def flatpage_sites(sender, instance, raw, **kwargs):
     if not raw:
         instance.sites = [Site.objects.get_current()]
 
-post_save.connect(flatpage_sites, sender=get_page_model())
+
+if getattr(settings, 'BASIC_PAGE_MODEL', DEFAULT_MODEL) == DEFAULT_MODEL:
+    from blanc_basic_pages.pages.models import Page
+    post_save.connect(flatpage_sites, sender=Page)
