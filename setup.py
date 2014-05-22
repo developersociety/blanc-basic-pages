14a139fdfc817945c2910a9ea1770c2fd618c0ef
Alex Tomkins <tomkins@darkzone.net>
2014-05-22 16:33:48 +0100

    Improved setup.py, add in newer Python versions

--- setup.py
+++ setup.py
@@ -1,33 +1,31 @@
 #!/usr/bin/env python
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-
+from setuptools import setup, find_packages
 
 setup(
     name='blanc-basic-pages',
     version='0.2.1',
     description='Blanc Basic Pages for Django',
     long_description=open('README.rst').read(),
-    url='http://www.blanctools.com/',
+    url='https://github.com/blancltd/blanc-basic-pages',
     maintainer='Alex Tomkins',
-    maintainer_email='alex@hawkz.com',
+    maintainer_email='alex@blanc.ltd.uk',
     platforms=['any'],
     install_requires=[
         'django-mptt>=0.6.0',
         'django-mptt-admin==0.1.8',
     ],
-    packages=[
-        'blanc_basic_pages',
-        'blanc_basic_pages.templatetags',
-    ],
+    packages=find_packages(),
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
     ],
-    license='BSD-2',
+    license='BSD',
 )
