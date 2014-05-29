71a331e4c8d790a569a75a56f8f8d2414a3af791
Alex Tomkins <tomkins@darkzone.net>
2014-05-29 14:33:29 +0100

    Improved navigation tags

--- blanc_basic_pages/templatetags/page_tags.py
+++ blanc_basic_pages/templatetags/page_tags.py
@@ -1,4 +1,6 @@
 from django import template
+from django.core.urlresolvers import reverse
+from django.utils import six
 from blanc_basic_pages import get_page_model
 
 register = template.Library()
@@ -6,6 +8,15 @@ register = template.Library()
 
 @register.assignment_tag
 def get_root_pages(current_page=None):
+    # If given a string, this could either be:
+    # - A Django URL (which can be reversed)
+    # - A page URL
+    if isinstance(current_page, six.string_types):
+        try:
+            current_page = get_page_model().objects.get(url=reverse(current_page))
+        except get_page_model().DoesNotExist:
+            current_page = None
+
     page_list = []
 
     # Find the root page so the template can highlight it
@@ -21,15 +32,14 @@ def get_root_pages(current_page=None):
 
 
 @register.assignment_tag
-def get_pages_at_level(page_level, current_page):
-    page_and_ancestors = list(current_page.get_ancestors())
-    page_and_ancestors.append(current_page)
+def get_pages_at_level(current_page, level=1):
+    page_and_ancestors = list(current_page.get_ancestors(include_self=True))
 
     # Page isn't deep enough to show this level of navigation
-    if page_level > current_page.level + 1:
+    if level > current_page.level + 1:
         return []
 
-    parent_page = page_and_ancestors[page_level - 1]
+    parent_page = page_and_ancestors[level - 1]
     page_list = []
 
     for i in parent_page.get_children().filter(show_in_navigation=True):
