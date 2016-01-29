a0b909091ad0c088e8cbbe596daa448d72ba43a6
Alex Tomkins <tomkins@darkzone.net>
2016-01-29 15:52:32 +0000

    Additional navigation tags

--- blanc_basic_pages/templatetags/page_tags.py
+++ blanc_basic_pages/templatetags/page_tags.py
@@ -6,6 +6,23 @@ from ..models import Page
 register = template.Library()
 
 
+@register.filter
+def show_nav(pages):
+    if pages:
+        pages = [x for x in pages if x.show_in_navigation]
+    return pages
+
+
+@register.assignment_tag
+def get_active_page(current_page):
+    try:
+        return Page.objects.get(url=resolve_url(current_page))
+    except Page.DoesNotExist:
+        pass
+
+    return None
+
+
 @register.assignment_tag
 def get_root_pages(current_page=None):
     if current_page and not isinstance(current_page, Page):
@@ -52,3 +69,36 @@ def get_pages_at_level(current_page, level=1):
         page_list.append((i, i in page_and_ancestors))
 
     return page_list
+
+
+@register.assignment_tag
+def tree_from_root(current_page=None):
+    tree = None
+
+    if current_page and not isinstance(current_page, Page):
+        try:
+            current_page = Page.objects.get(url=resolve_url(current_page))
+        except Page.DoesNotExist:
+            current_page = None
+
+    if current_page:
+        root_page = current_page.get_root()
+        tree = root_page.get_descendants()
+
+    return tree
+
+
+@register.assignment_tag
+def get_page_ancestor_ids(current_page=None):
+    ancestors = []
+
+    if current_page and not isinstance(current_page, Page):
+        try:
+            current_page = Page.objects.get(url=resolve_url(current_page))
+        except Page.DoesNotExist:
+            current_page = None
+
+    if current_page:
+        ancestors = current_page.get_ancestors(include_self=True).values_list('id', flat=True)
+
+    return ancestors
