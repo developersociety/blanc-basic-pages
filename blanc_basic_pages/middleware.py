16fc8ef8133ac53949d75bfd85cf2f4190936fc7
Thomas Lundqvist <biljettshop@users.noreply.github.com>
2016-10-27 14:33:23 +0200

    Update middleware.py

--- blanc_basic_pages/middleware.py
+++ blanc_basic_pages/middleware.py
@@ -1,9 +1,16 @@
 from django.http import Http404
 
 from .views import page
+# Django 1.10 compatibility
+
+try:
+    from django.utils.deprecation import MiddlewareMixin
+    MiddlewareClass = MiddlewareMixin
+except ImportError:
+    MiddlewareClass = object
 
 
-class PageFallbackMiddleware(object):
+class PageFallbackMiddleware(MiddlewareClass):
     def process_response(self, request, response):
         if response.status_code != 404:
             return response  # No need to check for a page for non-404 responses.
