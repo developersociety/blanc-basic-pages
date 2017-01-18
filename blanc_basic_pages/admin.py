33653b5198bc719a41963a7d9552d5f877525e8d
Alex Tomkins <tomkins@darkzone.net>
2017-01-18 16:28:24 +0000

    Add an option to turn off the Hero Image field
    Some sites don't need/use it, so there's a toggle to disable it

--- blanc_basic_pages/admin.py
+++ blanc_basic_pages/admin.py
@@ -15,7 +15,11 @@ class PageAdmin(DjangoMpttAdmin):
     form = PageAdminForm
 
     def get_fieldsets(self, request, obj=None):
+        content = ['hero_image', 'content']
         advanced_options = ['template_name', 'published', 'login_required']
+
+        if not getattr(settings, 'PAGE_SHOW_HERO_IMAGE', True):
+            content.remove('hero_image')
 
         if not SHOW_LOGIN_REQUIRED:
             advanced_options.remove('login_required')
@@ -28,7 +32,7 @@ class PageAdmin(DjangoMpttAdmin):
                 'fields': ('parent', 'show_in_navigation'),
             }),
             ('Content', {
-                'fields': ('hero_image', 'content'),
+                'fields': content,
             }),
             ('Advanced options', {
                 'fields': advanced_options,
--- docs/settings.rst
+++ docs/settings.rst
@@ -34,3 +34,12 @@ password authentication - so we hide it.
 If your site needs certain pages to be protected so that only logged-in users
 are able to view them, change this to ``True`` and then the option will appear
 in the Django admin.
+
+PAGE_SHOW_HERO_IMAGE
+====================
+
+Default: ``True``
+
+Shows or hides the *Hero Image* field for pages in the Django admin. By default
+this is ``True``, however if the site has no need for hero images in pages then
+this can be turned off.
